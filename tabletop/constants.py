358283dfa08430ade71e93ec12c87a0ece1f1379
jellyho <jellyho@naver.com>
2025-09-03 08:39:08 +0000

    grp norm fixed

--- tabletop/constants.py
+++ tabletop/constants.py
@@ -9,7 +9,7 @@ DT = 0.04
 ALOHA_XML_DIR = str(pathlib.Path(__file__).parent.resolve()) + '/assets/aloha' # note: absolute path
 BENCHMARK_INFO_DIR = str(pathlib.Path(__file__).parent.resolve()) + '/benchmark_info' # note: absolute path
 
-ALOHA_GRIPPER_CLOSE = 0.002
+ALOHA_GRIPPER_CLOSE = 0.009
 ALOHA_GRIPPER_OPEN = 0.037
 
 ############################ Helper functions ############################
@@ -17,5 +17,5 @@ ALOHA_GRIPPER_OPEN = 0.037
 ALOHA_GRIPPER_UNNORMALIZE_FN = lambda x: (x + 1) / 2 * (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE) + ALOHA_GRIPPER_CLOSE
 
 ## Assume gripper input is normalized to [-1, 1] range
-ALOHA_GRIPPER_NORMALIZE_FN = lambda x: 2 * ((x - ALOHA_GRIPPER_CLOSE) / (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE) - 0.115) / (1.185 - 0.115) - 1
+ALOHA_GRIPPER_NORMALIZE_FN = lambda x: 2 * ((x - ALOHA_GRIPPER_CLOSE) / (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE)) - 1 # - 0.115) / (1.185 - 0.115) - 1
 ALOHA_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE)
