5f229e3d50221851d183fd3cb74b21a01c3cf747
Euijin <jin.Prelude@gmail.com>
2025-09-07 20:06:15 +0900

    Add PROPRIO_GRIPPER_CLOSE

--- tabletop/constants.py
+++ tabletop/constants.py
@@ -9,14 +9,14 @@ DT = 0.04
 ALOHA_XML_DIR = str(pathlib.Path(__file__).parent.resolve()) + '/assets/aloha' # note: absolute path
 BENCHMARK_INFO_DIR = str(pathlib.Path(__file__).parent.resolve()) + '/benchmark_info' # note: absolute path
 
-ALOHA_GRIPPER_CLOSE = 0.009
+ALOHA_GRIPPER_CLOSE = 0.002
+PROPRIO_GRIPPER_CLOSE = 0.009
 ALOHA_GRIPPER_OPEN = 0.037
-# ALOHA_GRIPPER_OPEN = 0.041
 
 ############################ Helper functions ############################
 ## Assume gripper is normalized to [-1, 1] range, unnorm to [0, 1] range
 ALOHA_GRIPPER_UNNORMALIZE_FN = lambda x: (x + 1) / 2 * (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE) + ALOHA_GRIPPER_CLOSE
 
 ## Assume gripper input is normalized to [-1, 1] range
-ALOHA_GRIPPER_NORMALIZE_FN = lambda x: 2 * ((x - ALOHA_GRIPPER_CLOSE) / (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE)) - 1 # - 0.115) / (1.185 - 0.115) - 1
-ALOHA_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (ALOHA_GRIPPER_OPEN - ALOHA_GRIPPER_CLOSE)
+ALOHA_GRIPPER_NORMALIZE_FN = lambda x: 2 * ((x - PROPRIO_GRIPPER_CLOSE) / (ALOHA_GRIPPER_OPEN - PROPRIO_GRIPPER_CLOSE)) - 1 # - 0.115) / (1.185 - 0.115) - 1
+ALOHA_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (ALOHA_GRIPPER_OPEN - PROPRIO_GRIPPER_CLOSE)
